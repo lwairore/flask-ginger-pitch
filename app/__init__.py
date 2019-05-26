4ecce3e2295b886a7f6c2f4d0696cd6912cd35cc
lwairore <kwairore@gmail.com>
2019-05-26 06:54:44 +0300

    Import LoginManager from flask-login extension. Create login_manager of LoginManager class. Set login_manager,session_protection to strong. Prefix the login endpoint with the auth blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+login_manager = LoginManager()
+login_manager.session.protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 
@@ -22,6 +26,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
