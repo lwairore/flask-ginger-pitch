66a602aad7e64d6913fcc5d7d795e085ea28d902
lwairore <kwairore@gmail.com>
2019-05-26 09:49:43 +0300

    Import FlaskForm, StringField, TextAreField ,,Required and SubmitField

---
+++ app/main/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Who am I ?.', validators=[Required()])
+    submit = SubmitField("Save changes")
