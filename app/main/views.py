f6f34df3aab0464e58d0263c369ed9447396a398
lwairore <kwairore@gmail.com>
2019-05-26 10:31:28 +0300

    Import the photos instance. Create a route that will proccess form submission reguest. Query the databse to pick a user with the same username we passed in. Use request function to check if any parameter with the name photo has been passed into the request. Use the save method to save the file. Then update profile_pic_path property in our users table amd store the path to the file. Finally we commit the changes to the database  and redirect the user back to the profile page.

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request, redirect, url_for, abort
 from flask_login import login_required
 from ..models import User
 from .forms import UpdateProfile
-from .. import db
+from .. import db, photos
 
 @main.route('/')
 def index():
@@ -42,4 +42,13 @@ def update_profile(uname):
 
     return render_template('profile/update.html', form=form)
 
-    
+@main.route('/user/<uname>/update/pic', methods=["POST"])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = 'photos/{}'.format(filename)
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
