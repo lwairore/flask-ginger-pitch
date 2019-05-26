86e1670e82a78f18f7955dcee4803b9a659c55c5
lwairore <kwairore@gmail.com>
2019-05-26 08:18:25 +0300

    Define register route view function.

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,3 +8,15 @@ from  .. import db
 def login():
     title = "Pitch | Sign-in"
     return render_template('auth/login.html')
+
+@auth.route('/register', methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    title = 'Pitch | New Account'
+    return render_template('/auth/register.html', registration_form=form)
+    
