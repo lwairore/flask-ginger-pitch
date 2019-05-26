5862a907a120f8c52f6cabb45621b66e2ff4b881
lwairore <kwairore@gmail.com>
2019-05-26 10:00:03 +0300

    Create update_profile() view function that takes in username and instantiates the UpdateProfile form class. Query the database to find a user with the same username. If the form is validated we update the content of the user.bio property to fill in what the user has submitted and redirect the user back to the profile page where he can see the new bio.

--- app/main/views.py
+++ app/main/views.py
@@ -24,4 +24,22 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user=user)
+
+@main.route('/user/<uname>/update', methods=["GET","POST"])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
+    
     
