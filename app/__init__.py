40e5ff9d5702bc29cf85b97459341d0ab61c9062
lwairore <kwairore@gmail.com>
2019-05-26 06:50:14 +0300

    Register Blueprint instance  in create_app(). Pass url_prefix argument that will add a prefix to all the routes registerd with auth blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,34 @@
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 bootstrap = Bootstrap()
-db = SQLAlchemy
+db = SQLAlchemy()
 
 
 def create_app(config_name):
+
     app = Flask(__name__)
 
     # Creating the app configurations
+
     app.config.from_object(config_options[config_name])
+    # config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+    
+
+
+
     return app
+
     
