f3b6929afecdd7ab013dd53e5ddca429e931a6bc
lwairore <kwairore@gmail.com>
2019-05-26 13:41:44 +0300

    Instantiate simpleMDE class

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_simplemde import SimpleMDE
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos', IMAGES)
+simple = SimpleMDE()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -26,6 +28,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    simple.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
