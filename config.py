b24860253b01583c2a2ca2a5f204e93159730a6b
lwairore <kwairore@gmail.com>
2019-05-26 05:45:42 +0300

    import os. Create Config base class. Inside Config; Declare SECRET_KEY, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS. Create subclass, ProdConfig with pass function. Create subclass DevConfig, with DEBUG mode to True.

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+class Config:
+    """
+    General parent configuration parent class
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karangu:lmzongolo8754@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    """
+
+    """
+    pass
+
+class DevConfig(Config):
+    """
+
+    """
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+}
+
