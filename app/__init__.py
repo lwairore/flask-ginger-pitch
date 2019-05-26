1ad3114b1f4552c4a5de46c60cbb62f8c96f7d15
lwairore <kwairore@gmail.com>
2019-05-26 05:40:48 +0300

    Import config_options form config. Inside create_app(), extract different configuration based on config_name.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
@@ -7,6 +8,8 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
     bootstrap.init_app(app)
