7a7339dd2ab54366a7b540dd21c569fe45f8861c
lwairore <kwairore@gmail.com>
2019-05-26 08:25:45 +0300

    Import BooleanField. Add BooleanField input field that will render a checkbox in our form.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, ValidationError,BooleanField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 
@@ -8,6 +8,7 @@ class RegistrationForm(FlaskForm):
     username = StringField("Enter your username", validators=[Required()])
     password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message="Passwords must match")])
     password_confirm = PasswordField("Confirm Passwords", validators=[Required()])
+    remember = BooleanField("Remember me")
     submit = SubmitField('Sign Up')
     def validate_email(self, data_field):
         if User.query.filter_by(email=data_field.data).first():
