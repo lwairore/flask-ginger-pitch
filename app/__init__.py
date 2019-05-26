0a9252431301e439d533586260c40bdfa3004ff0
lwairore <kwairore@gmail.com>
2019-05-26 10:12:18 +0300

    Import UploadSet class.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
-
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
-
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos', IMAGES)
+
 login_manager = LoginManager()
-login_manager.session.protection = 'strong'
+login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
