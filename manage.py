ae7c662621d58b6c0c79fd37eebc3729d05edd48
lwairore <kwairore@gmail.com>
2019-05-26 06:27:44 +0300

    Import User class. Pass User class to python shell.

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
 
 app = create_app('development')
@@ -10,7 +11,7 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=User)
 
 
 
