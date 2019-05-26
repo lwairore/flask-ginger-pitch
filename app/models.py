d479b2003e9e74b531bce44a400e55059ad59192
lwairore <kwairore@gmail.com>
2019-05-26 09:21:28 +0300

    Add two columns; bio and profile_pic_path.

--- app/models.py
+++ app/models.py
@@ -13,6 +13,9 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255), index=True)
     password_hash = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
 
     @property
     def password(self):
