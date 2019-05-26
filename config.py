0bbf4993e1ca347d0510e5a536baafc9157339fd
lwairore <kwairore@gmail.com>
2019-05-26 10:16:10 +0300

    Create new configuration, UPLOADED_PHOTOS_DEST, which specifies  app/static/photos location to store our photos.

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karangu:lmzongolo8754@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     @staticmethod
     def init_app(app):
