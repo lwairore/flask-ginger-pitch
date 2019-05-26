40a4250ab2131424762961d71eb95c75ed8f945f
lwairore <kwairore@gmail.com>
2019-05-26 08:37:27 +0300

    Import flash and request. Create an instance of LoginForm,login_form, and pass it into login.html. Then check if form is validated properly where we search for a user from our database with the email we receive from the form. Then, use verify_password method to confirm that the password entered matches with the password has stored in the database.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,21 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from  .. import db
 
 @auth.route('/login')
 def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).firts()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid Username or Password!')
+
     title = "Pitch | Sign-in"
-    return render_template('auth/login.html')
+    return render_template('auth/login.html', title=title)
 
 @auth.route('/register', methods=["GET","POST"])
 def register():
@@ -19,4 +27,3 @@ def register():
         return redirect(url_for('auth.login'))
     title = 'Pitch | New Account'
     return render_template('/auth/register.html', registration_form=form)
-    
