6d9179617c88777735271cde2b3e4c17f9fa27b9
lwairore <kwairore@gmail.com>
2019-05-26 06:39:34 +0300

    Import generate_password_hash, check_password_hash. Inside class User; Define password method that hashes the password input of the user. Define verify_password() method that verifies if the password entered by the user is equal to what it has has.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Columne(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
+    password_hash = db.Column(db.String(255))
 
-    def __repr__(self):
-        return 'User {}'.format(self.username)
-        
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
