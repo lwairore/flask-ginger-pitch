f2f9020f1ef4b712d018fd4cc4100650e94f28e3
lwairore <kwairore@gmail.com>
2019-05-26 08:29:41 +0300

    Create LoginForm class. In LoginForm class; create 3 input fields, email, password and checkbox for remember me.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,6 @@ class RegistrationForm(FlaskForm):
     username = StringField("Enter your username", validators=[Required()])
     password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message="Passwords must match")])
     password_confirm = PasswordField("Confirm Passwords", validators=[Required()])
-    remember = BooleanField("Remember me")
     submit = SubmitField('Sign Up')
     def validate_email(self, data_field):
         if User.query.filter_by(email=data_field.data).first():
@@ -16,3 +15,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, data_field):
         if User.query.filter_by(username=data_field.data).first():
             raise ValidationError('That Username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Sign In")
