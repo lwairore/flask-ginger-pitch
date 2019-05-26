839a7b17fa17f34092cedd4a2e87560f76ba3d16
lwairore <kwairore@gmail.com>
2019-05-26 06:26:35 +0300

    Passed db to python shell

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import create_app, db
+from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,7 +10,7 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
 
 
 
