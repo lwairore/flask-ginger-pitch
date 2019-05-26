230f09ef1fddc0f3c64f80a253e7b922e06df3ba
lwairore <kwairore@gmail.com>
2019-05-26 05:54:13 +0300

    Import db, Migrate, MigrateCommand. Create migrate instance of app. Add db command. Add python shell.

--- manage.py
+++ manage.py
@@ -1,10 +1,16 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
+migrate Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+@manager.shell
+def make_shell_context():
+    return dict(app=app)
 
 
 
