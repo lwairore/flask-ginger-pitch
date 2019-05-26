019db442db54d0d2b0d97e0028cdfe42a29c8434
lwairore <kwairore@gmail.com>
2019-05-26 05:36:07 +0300

    Import Bootsrap, Create an instance of Bootstrap, bootstrap. Inside create_app(), initializing bootstrap.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
     app = Flask(__name__)
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    
