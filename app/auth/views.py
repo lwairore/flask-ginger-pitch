95140a308297d717191ce20bb42fb14d09209fcd
lwairore <kwairore@gmail.com>
2019-05-26 08:13:30 +0300

    Import redirect, url_for, User class, RegistrationForm, and db.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import auth
+from ..models import User
+from .forms import RegistrationForm
+from  .. import db
 
 @auth.route('/login')
 def login():
+    title = "Pitch | Sign-in"
     return render_template('auth/login.html')
