df592da7d2e1eee1ba2049bd8d7d7d3a7a4cb2c0
lwairore <kwairore@gmail.com>
2019-05-26 07:00:39 +0300

    Register main Blueprint instance  in create_app().

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,9 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
     
