ef75db50f6cf45158c64de238f4d044c802443c1
lwairore <kwairore@gmail.com>
2019-05-26 09:28:46 +0300

    Import abort function, redirect, url_for. Create a dynamic route that is handled by profile view function. Query database to find the user according to the username password. If no user is fount the abort function is called and a 404 status code is returned as a response. If a user is found profile.html is rendered.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from . import main
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort
+from flask_login import login_required
+from ..models import Reviews, User
 
 @main.route('/')
 def index():
@@ -11,3 +13,13 @@ def index():
 def new_post():
     title = "Create Pitch"
     return render_template("new_pitch.html", title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
+    
