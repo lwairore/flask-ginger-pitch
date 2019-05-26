17ad384946bb75effdbb2ee458e6409cd600beb7
lwairore <kwairore@gmail.com>
2019-05-26 12:39:01 +0300

    Update how we define save_pitch instance.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from . import main
 from flask import render_template, request, redirect, url_for, abort
-from flask_login import login_required
-from ..models import User
-from .forms import UpdateProfile
+from flask_login import login_required, current_user
+from ..models import User, Pitch
+from .forms import UpdateProfile, PitchForm
 from .. import db, photos
 
 @main.route('/')
@@ -52,3 +52,31 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile', uname=uname))
+
+@main.route('/new/pitch', methods=["GET","POST"])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        pitch = form.pitch.data
+
+        # Update Pitch
+        new_pitch = Pitch(pitch_title=title, pitch_body=pitch, user=current_user)
+
+        # Save Pitch 
+        new_pitch.save_pitch()
+        return redirect(url_for('.user_pitch',uname=current_user))
+    title = "Pitch | New"
+    user = current_user
+    user_details = User.query.filter_by(username=user).first()
+    return render_template('new_pitch.html', title=title, pitch_form=form, user=user_details)
+
+@main.route('/<uname>/pitch>')
+@login_required
+def user_pitch(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("pitch/user_pitch.html", user=user)
