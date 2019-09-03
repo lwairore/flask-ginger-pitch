cfac0b6076abd5bf3b6ef73f6356ca5576136991
lwairore <kwairore@gmail.com>
2019-09-03 15:19:55 +0300

    Import mail_message function from email.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from ..models import User
 from .forms import RegistrationForm, LoginForm
 from  .. import db
 from flask_login import login_user, logout_user, login_required
+from ..email import mail_message
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
@@ -25,6 +26,7 @@ def register():
         user = User(email=form.email.data, username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
+        mail_message("Welcome to Ginger-Pitch", "email/welcome_user", user.email, user=user)
         return redirect(url_for('auth.login'))
     title = 'Pitch | New Account'
     return render_template('/auth/register.html', registration_form=form)
