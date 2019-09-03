21b0ef6fee429cb2572d780ed5c24b2064ff1e54
lwairore <kwairore@gmail.com>
2019-09-03 15:14:09 +0300

    Add gmail configuration.

--- config.py
+++ config.py
@@ -9,6 +9,11 @@ class Config:
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     @staticmethod
     def init_app(app):
