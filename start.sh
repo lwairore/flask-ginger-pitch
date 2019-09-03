db644a70042f6c74eef6ce56ef17752f804fbdf6
lwairore <kwairore@gmail.com>
2019-09-03 15:16:00 +0300

    Add start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-export SECRET_KEY="wow...what i challenging project!!"
+export SECRET_KEY="wow...what a challenging project!!"
+export MAIL_USERNAME="kwairore@gmail.com"
+export MAIL_PASSWORD="lmzongolo4280"
+
 
 python3.6 manage.py server
