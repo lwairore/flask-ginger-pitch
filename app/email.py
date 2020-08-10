0569ffa8896ac01e470a7a05aa8cb2c5a3b684e2
lwairore <kwairore@gmail.com>
2020-08-10 15:33:20 +0300

    - Update mail_message() function to use sendgrid to send emails instead of the default flask_mail.

--- app/email.py
+++ app/email.py
@@ -2,10 +2,21 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
-def mail_message(subject,template, to, **kwargs):
-    sender_email = os.environ.get("MAIL_USERNAME")
-    email = Message(subject, sender=sender_email, recipients=[to])
-    email.body = render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+def mail_message(subject, to, **kwargs):
+    sender_email = os.environ.get("SENDGRID_MAIL_USERNAME")
+    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+
+    subject = subject
+    message = "Hi there,\nThanks for signing up to keep in touch with Ginger-Pitch family.From now on, you'll get regular updates on the best pitch practices. \nIn the meantime, check out communities best to offer pitches. Here's to the start of a healthy digital relationship.\nCheers,\nGinger Pitch"
+    message = Mail(
+        from_email=sender_email,
+        to_emails=to,
+        subject=subject,
+        html_content=message
+    )
+    sg = SendGridAPIClient(SENDGRID_API_KEY)
+    response = sg.send(message)
+    return response
